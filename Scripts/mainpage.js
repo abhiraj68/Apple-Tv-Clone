b674dbdbdd4fb9a7d0bab97974f821b929637f73
abhiraj68 <abhirajrajappan@gmail.com>
2022-10-17 19:48:12 +0530

    login

--- Scripts/mainpage.js
+++ Scripts/mainpage.js
@@ -1,6 +1,68 @@
-import navbar from "../navbar and footer(home page)/component/navbar.js";
-// console.log(navbar)
-document.getElementById("navbar").innerHTML = navbar();
+
+
+var signIn = document.querySelector("#signin");
+var backdrop = document.querySelector(".backdrop");
+var closebtn = document.querySelector(".close-modal_btn");
+var body = document.querySelector("body");
+var modal = document.querySelector(".modal");
+var advert = document.querySelector("#advertisement");
+var jumboBtn = document.querySelector(".jumbotron-btn");
+var iframe = document.querySelector("#modal-iframe");
+
+function openModal() {
+  backdrop.style.display = "block";
+  modal.style.display = "block";
+  body.style.overflow = "hidden";
+  iframe.src = "sign-in.html";
+}
+
+function closeModal() {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
+  body.style.overflow = "auto";
+  iframe.src = "sign-in.html";
+}
+
+// jumboBtn.addEventListener("click", function () {
+//   openModal();
+//   iframe.src = "sign-in.html";
+// });
+
+// advert.addEventListener("click", function () {
+//   openModal();
+//   iframe.src = "sign-in.html";
+// });
+
+signIn.addEventListener("click", function () {
+  openModal();
+  iframe.src = "./signupandlogin/sign-in.html";
+});
+
+backdrop.addEventListener("click", function () {
+  closeModal();
+  iframe.src = "sign-in.html";
+});
+
+closebtn.addEventListener("click", function () {
+  closeModal();
+  iframe.src = "sign-in.html";
+});
+
+$(document).scroll(function () {
+  var myID = document.getElementById("advertisement");
+  var footer = document.querySelector("#footer");
+  var myScrollFunc = function () {
+    var y = window.scrollY;
+    if (y >= 100) {
+      myID.className = "advertisement show";
+      footer.style.paddingBottom = "5rem";
+    } else {
+      myID.className = "advertisement hide";
+      footer.style.paddingBottom = "";
+    }
+  };
+  window.addEventListener("scroll", myScrollFunc);
+});
 
 document.getElementById("glass").addEventListener("click", myfunc)
 function myfunc() {
@@ -1520,6 +1582,11 @@ document.getElementById("sago").addEventListener("click",sago)
     var id=1
     localStorage.setItem("name",name)
     localStorage.setItem("id",id)
-location.href="./movieReview//movieReview.html"
+location.href="./movieReview/movieReview.html"
 
   }
+
+
+
+
+  
--- navbar/component/navbar.js
+++ navbar/component/navbar.js
@@ -1,7 +1,7 @@
 function navbar() {
     return `
     <div id="logo">
-        <a href="#"><img src="https://tv.apple.com/assets/brands/Apple_TV_plus_logo_white-2721485772b7d06c4e33379ce990870e.svg" alt=""></a>
+        <a href="../index.html"><img src="https://tv.apple.com/assets/brands/Apple_TV_plus_logo_white-2721485772b7d06c4e33379ce990870e.svg" alt=""></a>
     </div>
     <div id="user_nav">
         <i class="fa-solid fa-magnifying-glass"></i>
@@ -10,9 +10,14 @@ function navbar() {
             <i class="fa-solid fa-microphone"></i>
         </button>
 
-        <img onclick="user_Login()"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7vc_Yl8ivCDmoe-ZhVAgSISHffH9OSL5fXBpb49Ax0WiJ6eCA-J7LB9RU-gXu3smgIQ8&usqp=CAU"
-            alt="">
+        <button class="landing_navbar_button d-flex">
+        <div class="logo_sigin m-1">
+          <i class="fa-solid fa-user"></i>
+        </div>
+        <div class="logo_text">
+          <a id="logo" href="signin.html">Sign In</a>
+        </div>
+      </button>
 
     </div>`;
 }
