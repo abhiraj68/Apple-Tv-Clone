ccc3ceb59aaeb54dd92bb958dbd017bbb1813957
BGNM4160 <sachi.sn23@gmail.com>
2022-10-17 09:18:51 +0530

    signin

--- scripts/signin-form.js
+++ scripts/signin-form.js
@@ -1,23 +1,31 @@
 var signInData = JSON.parse(localStorage.getItem("signInData")) || [];
 
-var parentDocument = window.parent.document;
-var iframe = parentDocument.querySelector("#modal-iframe");
+document.querySelector("myForm").addEventListener("submit", getData);
 
-var form = document.querySelector("#myForm");
-form.addEventListener("submit", function (event) {
+function getData(){
   event.preventDefault();
-  var fname = form.fname.value;
-  var lname = form.lname.value;
-  var bday = form.bday.value;
-  var email = form.email.value;
-  var pass = form.pass.value;
-  var obj = {};
-  obj.fname = fname;
-  obj.lname = lname;
-  obj.bday = bday;
-  obj.email = email;
-  obj.pass = pass;
-  signInData.push(obj);
-  localStorage.setItem("signInData", JSON.stringify(signInData));
-  iframe.src = "sign-in.html";
-});
+  var data= {
+    fname: document.getElementById("fname").value,
+    lname: document.getElementById("lname").value,
+    email: document.getElementById("email").value,
+    pass: document.getElementById("pass").value,
+   
+    
+  };
+  var flag = false;
+  for(var i=0;i<signInData.length;i++){
+    if(data.email === signInData[i].email){
+      flag = true;
+      break;
+    }
+  }
+  if(flag == true){
+    alert("User already exists !");
+  }
+  else{
+    signInData.push(data);
+    localStorage.setItem("signInData", JSON.stringify(signInData));
+    window.location.href = "sign-in.html";
+  }
+}
+
